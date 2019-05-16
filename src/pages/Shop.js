89b1819a52a3ecd6c576cd6fb84239838b9fcdd8
Noah A <nanderson815@gmail.com>
2019-05-15 20:46:47 -0400

    removed cart div from shop, was redundant

--- src/pages/Shop.js
+++ src/pages/Shop.js
@@ -3,32 +3,25 @@ import Cart from '../components/Cart';
 import Product from '../components/Product';
 
 const Shop = (props) => {
-    
-    return (
-      <div className="App">
-        <header className="App__header">
-          <div className="App__title">
-            <h1>Shop Our Bags:</h1>
-            
-          </div>
-        </header>
-        
-        <div className="Product-wrapper">
-          {props.data.shop.products.edges.map(product =>
-            <Product addVariantToCart={props.addVariantToCart} checkout={props.checkout} key={product.node.id.toString()} product={product.node} />
-          )}
-        </div>
 
-        <Cart
-          checkout={props.checkout}
-          isCartOpen={props.isCartOpen}
-          handleCartClose={props.handleCartClose}
-          updateQuantityInCart={props.updateQuantityInCart}
-          removeLineItemInCart={props.removeLineItemInCart}
-        />
+  return (
+    <div className="App">
+      <header className="App__header">
+        <div className="App__title">
+          <h1>Shop Our Bags:</h1>
+
+        </div>
+      </header>
+
+      <div className="Product-wrapper">
+        {props.data.shop.products.edges.map(product =>
+          <Product addVariantToCart={props.addVariantToCart} checkout={props.checkout} key={product.node.id.toString()} product={product.node} />
+        )}
       </div>
-    );
-  
+
+    </div>
+  );
+
 }
 
 export default Shop;
