37d0cc2ce42b6e2fcc9ab4edc75fc43cf27a4967
Noah A <nanderson815@gmail.com>
2019-05-23 21:36:20 -0400

    adding footer content'

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 const styles = theme => ({
     background: {
-        backgroundColor: "blue",
+        backgroundColor: "#58646D",
         minHeight: "50px"
+    },
+    text: {
+        color: 'White'
     }
 })
 
@@ -13,7 +17,17 @@ const Footer = (props) => {
 
     return (
         <footer className={classes.background}>
+            <Grid
+                container
+                justify="center"
+                alignItems="center"
+                style={{ minHeight: '50px' }}
 
+            >
+                <Grid item>
+                    <h3 className={classes.text}>Copywrite Rose & Liz 2019</h3>
+                </Grid>
+            </Grid>
         </footer>
     )
 }
