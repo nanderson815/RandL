03e31479528a4cb7363b7d90846891f4986c69da
Noah A <nanderson815@gmail.com>
2019-05-25 10:47:27 -0400

    doing about us page

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+
+
+const styles = theme => ({
+    header: {
+        height: "75vh",
+        background: "url('/images/aboutHead1.jpg')",
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "100%"
+    }
+});
 
 const About = (props) => {
+    const { classes } = props;
+
     return (
         <div>
-            
+            <header className={classes.header}>
+
+            </header>
         </div>
     )
 }
 
-export default About;
+export default withStyles(styles)(About);
