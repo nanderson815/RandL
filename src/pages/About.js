62a316c4d35e26c5092feacace4caf3ab932b92b
Noah A <nanderson815@gmail.com>
2019-05-29 20:08:44 -0400

    adding container

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 import PageBreak from "../components/PageBreak";
+import Container from '@material-ui/core/Container';
 
 const styles = theme => ({
     header: {
@@ -17,8 +18,8 @@ const styles = theme => ({
         fontFamily: "'Josefin Slab', serif"
     },
     gridItem: {
-        margin: "20px",
-        padding: "10px"
+        padding: "10px",
+        marginTop: "20px"
     }
 });
 
@@ -46,54 +47,61 @@ const About = (props) => {
 
             <main>
                 {/* Grid #1 - Sisters and Best Friends */}
-                <Grid
-                    container
-                    spacing={0}
-                    justify="center"
-                    alignItems="center"
-                    style={{ minHeight: '60vh' }}
-                >
-                    <Grid item xs={11} md={5} className={classes.gridItem}>
-                        <img alt="Kelly and Erin as children." style={{ maxWidth: "100%" }} src="/images/aboutHead1.jpg"></img>
-                    </Grid>
+                <Container>
+                    <Grid
+                        container
+                        spacing={0}
+                        justify="center"
+                        alignItems="center"
+                        style={{ minHeight: '60vh' }}
+                    >
+                        <Grid item xs={12} md={6} className={classes.gridItem}>
+                            <img alt="Kelly and Erin as children." style={{ maxWidth: "100%" }} src="/images/aboutHead1.jpg"></img>
+                        </Grid>
 
-                    <Grid item md={5} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
-                        <h1 style={{ fontWeight: "600" }}>Sisters & Best Friends</h1>
-                        <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
-                        </hr>
-                        <h2>Twin sisters, Erin and Kelly, co-founded Rose & Liz after working corporate jobs for a few years after college
-                            and finding that women were getting the short end of the stick in the laptop bag market.
-                            Deciding that women deserved more than just skimpy laptop sleeves and pocket-less tote bags for work,
+                        <Grid item md={6} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
+                            <h1 style={{ fontWeight: "600" }}>Sisters & Best Friends</h1>
+                            <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
+                            </hr>
+                            <h2>Twin sisters, Erin and Kelly, co-founded Rose & Liz after working corporate jobs for a few years after college
+                                and finding that women were getting the short end of the stick in the laptop bag market.
+                                Deciding that women deserved more than just skimpy laptop sleeves and pocket-less tote bags for work,
                             they set out to create their own vision of a practical, professional, and feminine work bag. </h2>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Container>
 
                 {/* Grid #2 - Erin */}
-                <Grid
-                    container
-                    spacing={0}
-                    justify="center"
-                    alignItems="center"
-                    style={{ minHeight: '60vh', backgroundColor: "#f1f2f3" }}
+                <Container
+                    maxWidth="xl"
+                    style={{ backgroundColor: "#f1f2f3"}}
                 >
-                    <Grid item md={5} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
-                        <h1>Erin <strong>[Rose]</strong> Donovan</h1>
-                        <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
-                        </hr>
-                        <h2>Erin worked at a large consulting firm and traveled frequently for work.
-                            Every week on airplanes, she saw women traveling with large tote bags,
-                            each stuffed with a sea of disorganized items and a laptop floating in the mess.
-                            Finding a fashionable but practical laptop bag was impossible, with the most functional bags sold
-                            in the Men's department.  Irritated by the lack of options for women,
+                    <Grid
+                        container
+                        spacing={0}
+                        justify="center"
+                        alignItems="center"
+                        style={{ minHeight: '60vh', backgroundColor: "#f1f2f3" }}
+                    >
+                        <Grid item md={6} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
+                            <h1>Erin <strong>[Rose]</strong> Donovan</h1>
+                            <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
+                            </hr>
+                            <h2>Erin worked at a large consulting firm and traveled frequently for work.
+                                Every week on airplanes, she saw women traveling with large tote bags,
+                                each stuffed with a sea of disorganized items and a laptop floating in the mess.
+                                Finding a fashionable but practical laptop bag was impossible, with the most functional bags sold
+                                in the Men's department.  Irritated by the lack of options for women,
                             she vented to her sister about the inequality and eventually channeled her frustration into product development. </h2>
-                    </Grid>
+                        </Grid>
 
-                    <Grid item xs={11} md={5} className={classes.gridItem}>
-                        <div style={{ maxHeight: "400px", width: "100%", overflow: "hidden" }}>
-                            <img alt="Kelly and Erin as children." src="/images/erinD.jpg"></img>
-                        </div>
+                        <Grid item xs={12} md={6} className={classes.gridItem}>
+                            <div style={{ maxHeight: "400px", width: "100%", overflow: "hidden" }}>
+                                <img alt="Kelly and Erin as children." src="/images/erinD.jpg"></img>
+                            </div>
+                        </Grid>
                     </Grid>
-                </Grid>
+                </Container>
 
                 {/* Grid #2 - Kelly */}
                 <Grid
@@ -123,6 +131,7 @@ const About = (props) => {
                 </Grid>
             </main>
         </div>
+
     )
 }
 
