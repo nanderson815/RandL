093f2f218e7895f1255912fb68f634d0920d9e6e
Noah A <nanderson815@gmail.com>
2019-05-30 22:42:12 -0400

    done refactoring about page

--- src/components/AboutContainer.js
+++ src/components/AboutContainer.js
@@ -1,8 +1,21 @@
 import React from 'react';
 import Container from '@material-ui/core/Container';
+import Grid from "@material-ui/core/Grid";
+import { withStyles } from '@material-ui/core/styles';
+
+const styles = theme => ({
+    subFont: {
+        fontSize: "3rem",
+        fontFamily: "'Josefin Slab', serif"
+    },
+    gridItem: {
+        padding: "10px",
+        marginTop: "20px"
+    }
+});
 
 const AboutContainer = (props) => {
-    ({ alt, desc, img, title } = { props })
+    const { classes, alt, desc, img, title } = props;
     if (props.imgSide === "left") {
         return (
             <Container
@@ -16,7 +29,9 @@ const AboutContainer = (props) => {
                     style={{ minHeight: '60vh' }}
                 >
                     <Grid item xs={12} md={6} className={classes.gridItem}>
-                        <img alt={alt} style={{ maxWidth: "100%" }} src={img}></img>
+                        <div style={{ maxHeight: "400px", width: "100%", overflow: "hidden" }}>
+                            <img alt={alt} style={{ maxWidth: "100%" }} src={img}></img>
+                        </div>
                     </Grid>
 
                     <Grid item md={6} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
@@ -29,33 +44,35 @@ const AboutContainer = (props) => {
             </Container>
         )
     } else {
-        <Container
-            maxWidth="xl"
-            style={{ backgroundColor: "#f1f2f3" }}
-        >
-            <Grid
-                container
-                spacing={0}
-                justify="center"
-                alignItems="center"
-                style={{ minHeight: '60vh', backgroundColor: "#f1f2f3" }}
+        return (
+            <Container
+                maxWidth="xl"
+                style={{ backgroundColor: "#f1f2f3" }}
             >
-                <Grid item md={6} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
-                    {title}
-                    <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
-                    </hr>
-                    <h2>{desc}</h2>
-                </Grid>
+                <Grid
+                    container
+                    spacing={0}
+                    justify="center"
+                    alignItems="center"
+                    style={{ minHeight: '60vh', backgroundColor: "#f1f2f3" }}
+                >
+                    <Grid item md={6} className={classes.gridItem + " " + classes.subFont} style={{ textAlign: "center" }}>
+                        {title}
+                        <hr style={{ border: "1px solid #bc8f8f", backgroundColor: "#bc8f8f", }}>
+                        </hr>
+                        <h2>{desc}</h2>
+                    </Grid>
 
-                <Grid item xs={12} md={6} className={classes.gridItem}>
-                    <div style={{ maxHeight: "400px", width: "100%", overflow: "hidden" }}>
-                        <img alt={alt} src={img}></img>
-                    </div>
+                    <Grid item xs={12} md={6} className={classes.gridItem}>
+                        <div style={{ maxHeight: "400px", width: "100%", overflow: "hidden" }}>
+                            <img alt={alt} src={img}></img>
+                        </div>
+                    </Grid>
                 </Grid>
-            </Grid>
-        </Container>
+            </Container>
+        )
     }
 
 }
 
-export default AboutContainer;
+export default withStyles(styles)(AboutContainer);
