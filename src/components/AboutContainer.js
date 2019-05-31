37602fbf094f963cf190b498fbd93b8e29c49cd0
Noah A <nanderson815@gmail.com>
2019-05-30 21:52:54 -0400

    refactoring about us

---
+++ src/components/AboutContainer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Container from '@material-ui/core/Container';
+
+const AboutContainer = (props) => {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+
+export default AboutContainer;
